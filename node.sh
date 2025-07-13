85c042f7510ac3d1c7111b822dcbe0fdf014dc84
Michael Habib <michael.habib@couchbase.com>
2025-07-13 14:54:13 -0500

    node.sh

---
+++ node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Default to 1 node if no argument provided
+COUNT=${1:-1}
+
+echo "Starting $COUNT node(s)..."
+
+for i in $(seq 1 $COUNT); do
+    echo "Starting node $i"
+    cargo run --release -p node-svc &
+done
+
+echo "All $COUNT node(s) started in background" 
